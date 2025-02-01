9a5efc84c8401cdb476fd9b03efc3311192ef456
sampath kovvali <118274231+sampath017@users.noreply.github.com>
2025-02-01 06:37:30 +0000

    Pending changes exported from your codespace

--- src/dataset.py
+++ src/dataset.py
@@ -1,6 +1,5 @@
 import torch
 from torch.utils.data import Dataset, DataLoader, random_split
-import lightning as L
 
 
 class ShakespearDataset(Dataset):
@@ -26,11 +25,12 @@ class ShakespearDataset(Dataset):
     def save_tokens(self):
         if not self.tokens_file.exists():
             data = torch.tensor(self.encode(self.text))
+            torch.serialization.add_safe_globals(data)
 
             torch.save(data, self.tokens_file)
 
     def load_tokens(self):
-        self.data = torch.load(self.tokens_file)
+        self.data = torch.load(self.tokens_file, weights_only=True)
 
     def __len__(self):
         return len(self.data) - (self.context_size + 1)
@@ -49,29 +49,3 @@ class ShakespearDataset(Dataset):
                 f"Index {idx} out of range for data length {len(self.data)} with block size {self.context_size}")
 
     def __repr__(self): return f"Dataset({self.context_size=})"
-
-
-class ShakespearDataModule(L.LightningDataModule):
-    def __init__(self, path, context_size=8, batch_size=64):
-        super().__init__()
-        self.path = path
-        self.context_size = context_size
-        self.batch_size = batch_size
-
-    def prepare_data(self):
-        ShakespearDataset(self.path, self.context_size)
-
-    def setup(self, stage):
-        if stage == "fit":
-            self.dataset = ShakespearDataset(self.path, self.context_size)
-            train_size = int(len(self.dataset) * 0.7)
-            val_size = len(self.dataset) - train_size
-
-            self.train_dataset, self.val_dataset = random_split(
-                self.dataset, [train_size, val_size])
-
-    def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
-
-    def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size)

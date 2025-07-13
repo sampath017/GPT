1128f0312c173fdb4bd9afe9967f2d02d970cded
sampath <118274231+sampath017@users.noreply.github.com>
2025-07-13 18:32:16 +0530

    Run on GPU

--- src/settings.py
+++ src/settings.py
@@ -1,36 +1,34 @@
 from pathlib import Path
+import torch
 
 # data_path = Path("/home/jl_fs/shards")
 data_path = Path("../data/shakespear.txt")
 logs_path = Path("../logs")
 logs_path.mkdir(exist_ok=True)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-project_name = "GPT"
-
-model = {
-    "name": "GPT",
-    "num_embds": 96,
-    "num_heads": 4,
-    "num_blocks": 4,
+config = {
+    "project_name": "GPT-mini",
+    "device": device,
+    "model": {
+        "name": "GPT",
+        "num_embds": 384,
+        "num_heads": 8,
+        "head_size": 48,
+        "num_blocks": 12,
+        "dropout": 0.2
+    },
+    "training": {
+        "max_steps": 5000,
+        "eval_interval": 100,
+        "eval_steps": 200
+    },
+    "dataset": {
+        "name": "tiny_shakespear",
+        "vocab_size": 65,
+        "block_size": 256,
+        "batch_size": 128,
+        "train_split": 0.7,
+        "val_split": 0.3
+    }
 }
-
-
-max_lr = 6e-4
-min_lr = max_lr * 0.1
-warmup_steps = 715
-# max_steps = 19073
-max_steps = 2
-max_epochs = 10
-
-dataset = {
-    "name": "Fineweb1B",
-    # "vocab_size": 50304, # optimized vocabsize
-    "vocab_size": 50257,
-    "context_size": 1024,
-    "batch_size": 32,
-    "total_batch_size": 524288,  # 2**19, ~0.5M, in number of tokens
-    "train_split": 0.7,
-    "val_split": 0.3
-}
-
-wandb_offline = False
